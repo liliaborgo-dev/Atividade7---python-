a73ef2a4426f08cee0dcbd05ff0fc08cc32a9bf9
liliaborgo-dev <liliafalcaoborgo@gmail.com>
2026-04-23 18:38:13 +0000

    salvando exercicio

---
+++ Questao1.py
@@ -0,0 +1,14 @@
+'''
+Criar um programa que exiba a frase "Sou um programa Python!" exatamente 10 
+vezes, numerando cada linha no início e no final. 
+
+contador = 0
+
+while contador < 10:
+        print("Sou um programa Python!")
+        contador += 1 
+        '''
+
+for n in range (1,11):
+    print(f"{n},Sou um programa Python!{n}")
+
